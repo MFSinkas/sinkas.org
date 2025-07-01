e1f10c19f28341a5d384dae10770c150ba201d8b
L2BeatSinkas <anastassis.oikonomopoulos@l2beat.com>
2025-07-02 02:16:56 +0300

    fixing white page

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
-  base: '/sinkas.org/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
